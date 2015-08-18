5691728cfba67bb6bd0679d32a0b99e0fcf284be
Elia Portnoy <artofelia@gmail.com>
2015-08-18 18:33:38 -0400

    compartments

--- simple_SIER.py
+++ simple_SIER.py
@@ -26,30 +26,44 @@ def model():
     bet = .5
     sig = .5
     gam = .5
+    ne = 3
+    ni = 3
     
     def f(p): #differential equation
-        s = p[0]
-        l1 = p[1]
-        l2 = p[2]
-        i1 = p[3]
-        i2 = p[4]
+        s = p[0] #susptible
+        lat = p[1:1+ne] #latents
+        inf = p[ne+1:1+ne+ni] #infected
+        np = []
+        #print 'all', p
+        #print 'lat', lat
+        #print 'inf', inf
          
-        f1 = -bet*s*(i1+i2) #ds
-        f2 = bet*s*(i1+i2)-sig*l1 #dl1
-        f3 = sig*(l1-l2) #dl2
-        f4 = sig*l2-sig*i1 #di1
-        f5 = gam*(i1-i2) #di2
+        np.append( -bet*s*inf.sum() ) #d suseptible
         
-        return array([f1,f2,f3,f4,f5])
+        np.append( bet*s*inf.sum()-sig*lat[0] ) #d latent1 
+        for i in range(1, ne):
+            np.append( sig*(lat[i-1]-lat[i]) ) #d latent i+1
+         
+        np.append( sig*lat[ne-1]-sig*inf[0] ) #d infected 1
+        for i in range(1, ni):
+            np.append( gam*(inf[i-1]-inf[i]) ) #d infected i+1
+    
+        return array(np)
         
-    x0 = array([100,0,0,1,0], dtype=float64) #initial vals
-    steps = 50
-    arrx = kutta(x0, f, steps, .1)
-    s1 = [e[0] for e in arrx]
-    s2 = [e[1] for e in arrx]
-    print s1
+    x0 = zeros((1+ne+ni, 1), dtype=float64) #initial vals
+    x0[0] = 1000
+    x0[ne+1] = 1
+    #print 'init', x0
+    
+    steps = 600
+    arrx = kutta(x0, f, steps, .01)
+    
+    sus = [e[0] for e in arrx]
+    lat1 = [e[1] for e in arrx]
+    inf1 = [e[ne+1] for e in arrx]
     t = range(0,steps)
-    plt.plot(t, s1, 'r--', t, s2, 'g--')
+    
+    plt.plot(t, sus, 'r--', t, lat1, 'g--', t, inf1)
     plt.ylabel('Suseptible')
     plt.xlabel('Time')
     plt.show()
