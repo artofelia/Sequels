61668a28b0061c8892eb2779da01197e4ea986cd
Elia Portnoy <artofelia@gmail.com>
2015-08-20 21:15:09 -0400

    added gen intervals

--- stochastic_SIER.py
+++ stochastic_SIER.py
@@ -15,12 +15,13 @@ def model():
     ne = 2 #number of latent compartments
     ni = 2 #number of inf compartments
     t = 0.0 #time
-    ent = 30.0; #end time
-    interval = 10 #size of discrete time interval
+    ent = 50; #end time
+    inter_size = int(1.0) #size of discrete time interval
+    inter = range(0, ent, inter_size) #discrete intervals
     
     
-    ipop = 10 #inital population
-    iinf = 2 #inital infection
+    ipop = 1000 #inital population
+    iinf = 3 #inital infection
     inf_time = array([-1]*ipop)#records time of infection for each person
     inf_time[0:iinf] = 0
     
@@ -36,10 +37,12 @@ def model():
     for i in range(1, ni):
         ppl.append([])
         
-    print 'ppl', ppl
+    #print 'ppl', ppl
     
     b_ppl = [0]*(iinf)+[None]*(ipop-iinf) #backwards interval for ppl
     f_ppl = [[]]*ipop #forwards interval for ppl
+    
+    b_dist = []
     
     while t < ent: #stochastic step
         s = len(ppl[0]) #numer of susptible
@@ -57,7 +60,7 @@ def model():
         lam_e = sig/ne * lat.sum()
         lam_i = gam/ni * inf.sum()
         lam = lam_s+lam_e+lam_i
-        print 'LAM', lam
+        #print 'LAM', lam
         tao = expovariate(lam) #next time step
         #why is mean,the number of unrecovered individuals?
         event = uniform(0, lam) #event type
@@ -65,7 +68,7 @@ def model():
         #print 'lams,lame,lami,lam,tao,event', lam_s,lam_e,lam_i,lam,tao,event
         
         if event < lam_s:
-            print 'event is SUS'
+            #print 'event is SUS'
             sus_person = int(choice(ppl[0])) #pick suseptible person
             ppl[0].remove(sus_person)
             ppl[1].append(sus_person)
@@ -79,41 +82,52 @@ def model():
             b_ppl[sus_person] = g
             f_ppl[inf_person].append(g)
             
-            
-            #change_person = choice( concatenate( [ppl[i] for i in range(1+ne,1+ne+ni) ] ) ) #pick person to move
         elif event <= lam_s+lam_e:
             
             #move person in one of latent compartments
             comp = 1+choice( [i for i, j in enumerate(map(bool, ppl[1:ne+1] )) if j] ) #pick nonempty latent compartment
 
-            print 'event is LATENT', ppl[comp]
+            #print 'event is LATENT', ppl[comp]
             chosen = choice( ppl[comp] )
             ppl[comp].remove(chosen)
             ppl[comp+1].append(chosen)
         else:
             
             #move person in one of infected compartments
-            print 'event is INF',  ppl[1+ne:1+ne+ni]
             comp = 1+ne+choice( [i for i, j in enumerate(map(bool, ppl[1+ne:1+ne+ni] )) if j] )
+            #print 'event is INF',  ppl[1+ne:1+ne+ni]
 
             chosen = choice( ppl[comp] )
             ppl[comp].remove(chosen)
             if comp!=ne+ni:
                 ppl[comp+1].append(chosen)
         t = t+tao
-        print 'end step', t
-        print 'ppl status', ppl
-    print "B DIST", b_ppl
+        #print 'end step', t
+        #print 'ppl status', ppl
     
-    #sus = [e[0] for e in arrx]
-    #lat1 = [e[1] for e in arrx]
-    #inf1 = [e[ne+1] for e in arrx]
-    #t = range(0,steps)
-    
-    #plt.plot(t, sus, 'r--', t, lat1, 'g--', t, inf1)
-    #plt.ylabel('Suseptible')
-    #plt.xlabel('Time')
-    #plt.show()
+    batt = 5 #look at bdist at discrete time batt
+    bdistt = [] #discrete backwards dist at time batt
+    for i in range(ipop):
+        if not b_ppl[i]:
+            continue
+        tdic = int( (inf_time[i]-(inf_time[i]%inter_size)) / inter_size ) #discrete time of infection
+        if tdic==batt-1 : #pick out individuals who got sick at batt
+            gdic = int( (b_ppl[i]-(b_ppl[i]%inter_size)) / inter_size ) #dicrete size of gen interval
+            bdistt.append(gdic)
+            
+    fdistt = [] #discrete forward dist at time batt
+    for i in range(ipop):
+        if not f_ppl[i]:
+            continue
+        tdic = int( (inf_time[i]-(inf_time[i]%inter_size)) / inter_size ) #discrete time of infection
+        if tdic==batt-1 :
+            for e in f_ppl[i]:
+                gdic = int( (e-(e%inter_size)) / inter_size )
+                fdistt.append(gdic) 
+        
+    plt.hist(fdistt,bins=50)
+
+    plt.show()
       
 model()
     
